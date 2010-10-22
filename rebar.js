14c64cb49c7b39c0b46c7197d80022970d530862
Andrew Hay Kurtz <andrew.hay.kurtz@gmail.com>
2010-10-22 11:34:02 -0700

    basic defing and building

--- rebar.js
+++ rebar.js
@@ -1,3 +1,31 @@
+
+var Rebar = function() {
+  this.definitions = [];
+};
+
+Rebar.prototype.def = function(factoryName, schema) {
+  this.definitions[factoryName] = schema;
+};
+
+Rebar.prototype.build = function(factoryName) {
+  var factory = this.definitions[factoryName]
+  var widget = {};
+  for(var field in factory) {
+    if (!factory.hasOwnProperty(field)) continue;
+    typeof(factory[field]) === "function" ?
+      widget[field] = factory[field]() : widget[field] = factory[field];
+  }
+  return widget;
+};
+
 window.onload = function(){
-  console.log('rebar!');
+  var rb = new Rebar()
+  rb.def('larry', {
+    name : 'larry b',
+    age  : function() {
+      return Math.floor(Math.random() * 100);
+    }
+  })
+
+  console.log(rb.build('larry'))
 };
